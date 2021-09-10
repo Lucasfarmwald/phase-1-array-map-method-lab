3ab82995c1bddc81ead7925447ba9ab31930f28a
lucasfarmwald <farmwaldlucas@gmail.com>
2021-09-10 09:52:25 -0500

    Done.

--- index.js
+++ index.js
@@ -11,8 +11,4 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-//const titleCased = tutorials.map(() => (tutorials))
-
-function titleCased() {
-  return tutorials
-}
+const titleCased = () => tutorials.map(tutorial => tutorial.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' '))
